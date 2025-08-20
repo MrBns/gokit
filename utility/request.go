0b90ee82f1c875b9c5773e90d7e89017b6c9ec99
Mrbns <hi@mrbns.dev>
2025-08-21 03:51:07 +0600

    added safely decode json body method helper

---
+++ utility/request.go
@@ -0,0 +1,28 @@
+package bns
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/mrbns/gokit/berr"
+)
+
+// Safely decoe body. becuase this fucntion will check if body is not empty.
+//
+// Return a  berr.ErrorMap map instance.
+func SafeDecodeBody_Json(r *http.Request, v any) berr.ErrorMap {
+	errorMap := berr.NewErrorMap()
+
+	if r.Body == http.NoBody {
+		errorMap.Set("body", fmt.Errorf("request body is empty"))
+		return errorMap
+	}
+
+	err := json.NewDecoder(r.Body).Decode(v)
+	if err != nil {
+		errorMap.Set("body-decode", err)
+	}
+
+	return errorMap
+}
